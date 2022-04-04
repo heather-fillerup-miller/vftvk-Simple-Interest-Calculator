ba0f0e8035be5ae32104b562a35b7120a938c558
Heather Miller <Heather.Miller@ibm.com>
2022-04-04 09:02:27 -0500

    Exercise 5

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function compute()
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years");
     var interest = (principal * rate) / 100;
-    var year = parseInt(new Date().getFullYear());
+    var year = Date.now().getFullYear();
     year += parseInt(years);
     result = document.getElementById("result").innerHTML= "If you deposit " + principal + 
     ", \<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of " + interest +
