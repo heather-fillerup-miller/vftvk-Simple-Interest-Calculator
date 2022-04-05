d8e05eac22f5f98b10d3b9aad52baca61f538654
Heather Miller <Heather.Miller@ibm.com>
2022-04-05 09:10:40 -0500

    Exercise 5

--- script.js
+++ script.js
@@ -3,12 +3,9 @@ function compute()
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = parseInt(document.getElementById("years").value);
-    console.log("The extracted year is: " + years + " type is: " + typeof(years));
-    var interest = principal * years * (rate / 100);
-    var amount = principal + interest;
+    var amount = principal * (1 + (years * rate / 100));
     var year = new Date().getFullYear();
-    console.log("The current year type: " + year + "type is: " + typeof(year));
-    year += years;
+    
     result = document.getElementById("result").innerHTML= "If you deposit " + principal + 
     ", \<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of " + amount +
      " in the year " + year + "\<br>";
